cb0d2d9373941a5fd8f5adf7e7904d5def248138
Chris Peterson <chris.peterson@gettyimages.com>
2021-07-07 14:00:12 -0700

    Bug fixes for gitlab search

--- src/GitlabCli/Search.psm1
+++ src/GitlabCli/Search.psm1
@@ -13,28 +13,38 @@ function Search-GitLab {
         $MergeRequests,
 
         [Parameter(Mandatory=$false)]
+        [uint]
+        $MaxResults = 100,
+
+        [Parameter(Mandatory=$false)]
+        [switch]
+        $All,
+
+        [Parameter(Mandatory=$false)]
         [switch]
         $WhatIf
     )
 
-    if($Blobs) {
-        if($WhatIf) {
-            Invoke-GitLabSearch "blobs" $Phrase -Whatif
-        } 
-        else {
-            Invoke-GitLabSearch "blobs" $Phrase | Get-GitLabApiPagedResults -All |
-                Foreach-Object { $_ | New-WrapperObject }
-        }
-    } elseif($MergeRequests) {
-        if($WhatIf) {
-            Invoke-GitLabSearch "merge_requests" $Phrase -WhatIf
-        } 
-        else {
-            Invoke-GitLabSearch "merge_requests" $Phrase | Get-GitLabApiPagedResults -All |
-                Foreach-Object { $_ | New-WrapperObject }
-        }
+    if ($Blobs) {
+        $Scope = 'blobs'
+    } elseif ($MergeRequests) {
+        $Scope = 'merge_requests'
     } else {
         throw "Must search either blobs OR merge requests"
+    }
+
+    if ($All) {
+        if ($MaxResults) {
+            Write-Warning -Message "Ignoring -MaxResults in favor of -All"
+        }
+        $MaxResults = [int]::MaxValue
+    }
+
+    if($WhatIf) {
+        Invoke-GitLabSearch -Scope $Scope -Phrase $Phrase -MaxResults $MaxResults -WhatIf
+    } 
+    else {
+        Invoke-GitLabSearch -Scope $Scope -Phrase $Phrase -MaxResults $MaxResults
     }
 }
 
@@ -49,6 +59,10 @@ function Invoke-GitLabSearch {
         [string]
         $Phrase,
 
+        [Parameter(Mandatory=$true)]
+        [uint]
+        $MaxResults,
+
         [Parameter(Mandatory=$false)]
         [switch]
         $WhatIf
@@ -57,55 +71,41 @@ function Invoke-GitLabSearch {
     $GitlabConfig = Get-GitLabCliConfig
 
     $RequestParameters = @{
-        Method="GET"
-        Uri= "$($GitlabConfig.Url)/api/v$($GitlabConfig.ApiVersion)/search"
-        Headers=@{ "PRIVATE-TOKEN"=$GitlabConfig.Token }
+        Method = "GET"
+        Uri = "$($GitlabConfig.Url)/api/v$($GitlabConfig.ApiVersion)/search?per_page=$($GitlabConfig.PageSize)&scope=$Scope&search=$([System.Web.HttpUtility]::UrlEncode($Phrase))"
+        Headers = @{ "PRIVATE-TOKEN" = $GitlabConfig.Token }
     }
 
-    $RequestParameters.Uri += "?scope=$Scope&search=" + [System.Web.HttpUtility]::UrlEncode($Phrase)
+    $MaxPages = [Math]::Max(1, $MaxResults / $GitlabConfig.PageSize)
 
     if ($WhatIf) {
-        Write-Host "WhatIf: $($RequestParameters | ConvertTo-Json)"
+        Write-Host "WhatIf: $($RequestParameters | ConvertTo-Json) -- up to $MaxPages pages of results"
     } else {
-        $Results = Invoke-RestMethod @RequestParameters
+        Invoke-RestMethod -Uri $RequestParameters.Uri -Method $RequestParameters.Method -Headers $RequestParameters.Headers -FollowRelLink -MaximumFollowRelLink $MaxPages |
+            ForEach-Object { # Page
+                $_ | ForEach-Object { # Result
+                    $_ | New-WrapperObject }
+            }
     }
-    $Results
-}
-
-function Get-GitLabApiPagedResults {
-    [CmdletBinding()]
-    param(
-        [Parameter(ValueFromPipeline=$true)]
-        $GitlabResponse,
-
-        [Parameter(Mandatory=$false)]
-        [switch]
-        $All
-    )
-
-    $GitlabConfig = Get-GitLabCliConfig
-    $BaseSearchRequestParameters = @{
-        Method="GET"
-        Headers=@{ "PRIVATE-TOKEN"=$GitlabConfig.Token }
-    }
-
-    while ($GitlabResponse.RelationLink.next) {
-        Write-Verbose "Getting Next Page $($GitlabResponse.RelationLink.next)"
-        $LoopParams = $BaseSearchRequestParameters + @{
-            Uri = $GitlabResponse.RelationLink.next
-        }
-        $GitlabResponse = Invoke-RestMethod @LoopParams
-        $GitlabResponse.Content | ConvertFrom-Json
-    }  
 }
 
 function Get-GitLabCliConfig {
     $Captures = (Get-Content '~/.python-gitlab.cfg' |
-        Select-String -Pattern 'private_token = (?<Token>.*)','url = (?<Url>.*)','api_version = (?<ApiVersion>.*)' -AllMatches).Matches.Groups | Where-Object -Property Name -ne "0" 
-    $Result = New-Object PSObject
+        Select-String -Pattern 'private_token = (?<Token>.*)','url = (?<Url>.*)','api_version = (?<ApiVersion>.*)','per_page = (?<PageSize>.*)' -AllMatches).Matches.Groups | Where-Object -Property Name -ne "0" 
+    $Config = New-Object PSObject
     $Captures | ForEach-Object {
-        $Result | Add-Member -NotePropertyName $_.Name -NotePropertyValue $_.Value.TrimEnd()
+        $Config | Add-Member -NotePropertyName $_.Name -NotePropertyValue $_.Value
     }
 
-    $Result
+    # perform any normalization / sanitization
+    $Config.Url = $Config.Url.TrimEnd('/')
+    if ([string]::IsNullOrWhiteSpace($Config.ApiVersion)) {
+        Write-Debug -Message "Defaulting api_version to 4"
+        $Config.ApiVersion = '4' # latest version of API
+    }
+    if ([string]::IsNullOrWhiteSpace($Config.PageSize)) {
+        $Config.PageSize = '20' # this is the GitLab API default
+    }
+
+    $Config
 }
