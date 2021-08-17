d3bb8b7afa47c89766295d9f98f037199e2dd80b
Casey MacPherson <casey@macphersonclan.org>
2021-08-17 12:40:03 -0700

    fixing consistency issue with cmdlet use

--- src/GitlabCli/Git.psm1
+++ src/GitlabCli/Git.psm1
@@ -16,12 +16,12 @@ function Get-LocalGitContext {
         if (Get-ChildItem -Filter '.git' -Hidden -Directory) {
             $Context.Repo = $($(git config --get remote.origin.url) -split ':' | Select-Object -Last 1).Replace('.git', '')
             
-            $branchOrSha = git status | select-string "^HEAD detached at (?<sha>.{8})`|^On branch (?<branch>.*)"
+            $branchOrSha = git status | Select-String "^HEAD detached at (?<sha>.{8})`|^On branch (?<branch>.*)"
  
             if($branchOrSha.Matches[0].Groups["sha"].Success) {
                 $Context.Branch = (git branch -a --contains $branchOrSha.Matches[0].Groups["sha"].Value `
                     | Select-Object -Skip 1 -First 1 `
-                    | select-string "  (?<branch>.*)").Matches[0].Groups["branch"].Value
+                    | Select-String "  (?<branch>.*)").Matches[0].Groups["branch"].Value
             } elseif ($branchOrSha.Matches[0].Groups["branch"]) {
                 $Context.Branch = $branchOrSha.Matches[0].Groups["branch"].Value
             } else {
