2d5005679ae82759b46402fa74cb98df54cb77a6
Chris Peterson <chris.peterson@gettyimages.com>
2021-11-18 12:42:20 -0800

    yaml enhancements

--- src/GitlabCli/GitlabCli.psd1
+++ src/GitlabCli/GitlabCli.psd1
@@ -4,7 +4,7 @@
     # RootModule = ''
     
     # Version number of this module.
-    ModuleVersion = '1.22.2'
+    ModuleVersion = '1.22.3'
     
     # Supported PSEditions
     # CompatiblePSEditions = @()
@@ -119,6 +119,7 @@
 
         # Repository Files
         'Get-GitlabRepositoryFileContent'
+        'Get-GitlabRepositoryYmlFileContent'
         'Get-GitlabCiYml'
 
         # Environments
--- src/GitlabCli/RepositoryFiles.psm1
+++ src/GitlabCli/RepositoryFiles.psm1
@@ -9,14 +9,14 @@ function Get-GitlabRepositoryFileContent {
         [string]
         $ProjectId = '.',
 
+        [Parameter(Position=0, Mandatory=$true)]
+        [string]
+        $FilePath,
+
         [Parameter(Mandatory=$false)]
         [Alias("Branch")]
         [string]
         $Ref,
-
-        [Parameter(Position=0, Mandatory=$true)]
-        [string]
-        $FilePath,
 
         [Parameter(Mandatory=$false)]
         [string]
@@ -33,7 +33,7 @@ function Get-GitlabRepositoryFileContent {
     }
     $RefName = $(Get-GitlabBranch -ProjectId $ProjectId -Ref $Ref).Name
 
-    $File = Invoke-GitlabApi GET "projects/$($Project.Id)/repository/files/$($FilePath)?ref=$RefName" -SiteUrl $SiteUrl -WhatIf:$WhatIf
+    $File = Invoke-GitlabApi GET "projects/$($Project.Id)/repository/files/$($FilePath | ConvertTo-UrlEncoded)?ref=$RefName" -SiteUrl $SiteUrl -WhatIf:$WhatIf
     
     if ($File -and $File.encoding -eq 'base64') {
         [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($File.content))
@@ -61,11 +61,40 @@ function Get-GitlabCiYml {
         $WhatIf
     )
 
-    $Yml = $(Get-GitlabRepositoryFileContent -ProjectId $ProjectId -Ref $Ref -FilePath '.gitlab-ci.yml' -SiteUrl $SiteUrl -WhatIf:$WhatIf)
+    Get-GitlabRepositoryYmlFileContent -ProjectId $ProjectId -FilePath '.gitlab-ci.yml' -Ref $Ref -SiteUrl $SiteUrl -WhatIf:$WhatIf
+}
+
+function Get-GitlabRepositoryYmlFileContent {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false)]
+        [string]
+        $ProjectId = '.',
+
+        [Parameter(Position=0, Mandatory=$true)]
+        [string]
+        $FilePath,
+
+        [Parameter(Mandatory=$false)]
+        [Alias("Branch")]
+        [string]
+        $Ref,
+
+        [Parameter(Mandatory=$false)]
+        [string]
+        $SiteUrl,
+
+        [switch]
+        [Parameter(Mandatory=$false)]
+        $WhatIf
+    )
+
+    $Yml = $(Get-GitlabRepositoryFileContent -ProjectId $ProjectId -Ref $Ref -FilePath $FilePath -SiteUrl $SiteUrl -WhatIf:$WhatIf)
     $Hash = $([YamlDotNet.Serialization.Deserializer].GetMethods() |
         Where-Object { $_.Name -eq 'Deserialize' -and $_.ReturnType.Name -eq 'T' -and $_.GetParameters().ParameterType.Name -eq 'String' }). `
         MakeGenericMethod(
             [object]). `
         Invoke($(New-Object 'YamlDotNet.Serialization.Deserializer'), $Yml)
-    New-Object 'psobject' -Property $Hash
+
+    return $Hash | ConvertTo-Json -Depth 100 | ConvertFrom-Json # the JSON "double-tap" coerces HashTables into PSCustomObject (including nested children)
 }
