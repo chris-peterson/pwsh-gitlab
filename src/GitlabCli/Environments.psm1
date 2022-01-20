ad782bd32d83edda0f3e1c58bfc4b8340872e2df
Andy Alm <andy.alm@gettyimages.com>
2022-01-19 18:18:31 -0800

    Added a command for stopping and deleting an environment. Also fixed a bug and added pagination support to the Get-GitlabEnvironment command

--- src/GitlabCli/Environments.psm1
+++ src/GitlabCli/Environments.psm1
@@ -1,8 +1,8 @@
 function Get-GitlabEnvironment {
-    [CmdletBinding(DefaultParameterSetName='ProjectId')]
+    [CmdletBinding(DefaultParameterSetName='List')]
     [Alias('envs')]
     param (
-        [Parameter(ParameterSetName='ProjectId', Mandatory=$false)]
+        [Parameter(Mandatory=$false)]
         [string]
         $ProjectId = '.',
 
@@ -17,6 +17,10 @@ function Get-GitlabEnvironment {
         [Parameter(Mandatory=$false)]
         [switch]
         $IncludeStopped,
+
+        [Parameter(Mandatory=$false)]
+        [int]
+        $MaxPages = 1,
 
         [Parameter(Mandatory=$false)]
         [string]
@@ -34,6 +38,7 @@ function Get-GitlabEnvironment {
         Path="projects/$($Project.Id)/environments"
         Query=@{}
         SiteUrl = $SiteUrl
+        MaxPages = $MaxPages
     }
 
     switch ($PSCmdlet.ParameterSetName) {
@@ -45,9 +50,81 @@ function Get-GitlabEnvironment {
         }
     }
 
-    if (-not $IncludeStopped) {
+    if ((-not $IncludeStopped) -and (-not $Name)) {
         $GitlabApiArguments.Query['states'] = 'available'
     }
 
     Invoke-GitlabApi @GitlabApiArguments -WhatIf:$WhatIf | New-WrapperObject 'Gitlab.Environment'
+}
+
+function Stop-GitlabEnvironment {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [string]
+        $ProjectId = '.',
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string]
+        $Name,
+
+        [Parameter(Mandatory=$false)]
+        [string]
+        $SiteUrl,
+
+        [switch]
+        [Parameter(Mandatory=$false)]
+        $WhatIf
+    )
+
+    process {
+        $Project = Get-GitlabProject -ProjectId $ProjectId
+        $Environment = Get-GitlabEnvironment -ProjectId $Project.Id -Name $Name
+    
+        $GitlabApiArguments = @{
+            HttpMethod='POST'
+            Path="projects/$($Project.Id)/environments/$($Environment.Id)/stop"
+            SiteUrl = $SiteUrl
+        }
+    
+        Invoke-GitlabApi @GitlabApiArguments -WhatIf:$WhatIf | Out-Null
+
+        Write-Host "Environment '$($Environment.Name)' (id: $($Environment.Id)) has been stopped"
+    }
+}
+
+function Remove-GitlabEnvironment {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [string]
+        $ProjectId = '.',
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string]
+        $Name,
+
+        [Parameter(Mandatory=$false)]
+        [string]
+        $SiteUrl,
+
+        [switch]
+        [Parameter(Mandatory=$false)]
+        $WhatIf
+    )
+
+    process {
+        $Project = Get-GitlabProject -ProjectId $ProjectId
+        $Environment = Get-GitlabEnvironment -ProjectId $Project.Id -Name $Name
+    
+        $GitlabApiArguments = @{
+            HttpMethod='DELETE'
+            Path="projects/$($Project.Id)/environments/$($Environment.Id)"
+            SiteUrl = $SiteUrl
+        }
+    
+        Invoke-GitlabApi @GitlabApiArguments -WhatIf:$WhatIf | Out-Null
+    
+        Write-Host "Environment '$($Environment.Name)' (id: $($Environment.Id)) has been deleted"
+    }
 }
