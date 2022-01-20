25aee36d102902ed704e94188beb14c7236d25de
Andy Alm <andy.alm@gettyimages.com>
2022-01-19 17:29:13 -0800

    Added the ability to query a deployment by its id

--- src/GitlabCli/Deployments.psm1
+++ src/GitlabCli/Deployments.psm1
@@ -1,36 +1,41 @@
 # https://docs.gitlab.com/ee/api/deployments.html#list-project-deployments
 function Get-GitlabDeployment {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="Query")]
     [Alias('deploys')]
     param (
         [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
         [string]
         $ProjectId = '.',
 
-        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, ParameterSetName="Query")]
         [string]
         $EnvironmentName,
 
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory=$false, ParameterSetName="Query")]
         [ValidateSet('created', 'running', 'success', 'failed', 'canceled', 'all')]
         [string]
         $Status = 'success',
 
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory=$false, ParameterSetName="Query")]
         [switch]
         $Latest,
 
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory=$false, ParameterSetName="Query")]
         [string]
         $UpdatedBefore,
 
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory=$false, ParameterSetName="Query")]
         [string]
         $UpdatedAfter,
 
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory=$false, ParameterSetName="Query")]
         [int]
         $MaxPages = 1,
+
+        [Parameter(Mandatory=$true, ParameterSetName="ById")]
+        [Alias("Id")]
+        [string]
+        $DeploymentId,
 
         [Parameter(Mandatory=$false)]
         [switch]
@@ -49,25 +54,34 @@ function Get-GitlabDeployment {
 
     $GitlabApiArguments = @{
         HttpMethod = 'GET'
-        Path = "projects/$($Project.Id)/deployments"
-        Query = @{
-            sort='desc'
-        }
-        MaxPages = $MaxPages
         SiteUrl = $SiteUrl
     }
-
-    if ($EnvironmentName) {
-        $GitlabApiArguments.Query['environment'] = $EnvironmentName
-    }
-    if ($Status -and $Status -ne 'all') {
-        $GitlabApiArguments.Query['status'] = $Status
-    }
-    if ($UpdatedBefore) {
-        $GitlabApiArguments.Query['updated_before'] = $UpdatedBefore
-    }
-    if ($UpdatedAfter) {
-        $GitlabApiArguments.Query['updated_after'] = $UpdatedAfter
+    switch($PSCmdlet.ParameterSetName) {
+        Query {
+            $GitlabApiArguments.Path = "projects/$($Project.Id)/deployments"
+            $GitlabApiArguments.Query = @{
+                sort='desc'
+            }
+            $GitlabApiArguments.MaxPages = $MaxPages
+            if ($EnvironmentName) {
+                $GitlabApiArguments.Query['environment'] = $EnvironmentName
+            }
+            if ($Status -and $Status -ne 'all') {
+                $GitlabApiArguments.Query['status'] = $Status
+            }
+            if ($UpdatedBefore) {
+                $GitlabApiArguments.Query['updated_before'] = $UpdatedBefore
+            }
+            if ($UpdatedAfter) {
+                $GitlabApiArguments.Query['updated_after'] = $UpdatedAfter
+            }
+        }
+        ById {
+            $GitlabApiArguments.Path = "projects/$($Project.Id)/deployments/$DeploymentId"
+        }
+        default {
+            throw "Parameterset $($PSCmdlet.ParameterSetName) is not implemented"
+        }
     }
 
     $Result = Invoke-GitlabApi @GitlabApiArguments -WhatIf:$WhatIf | New-WrapperObject 'Gitlab.Deployment'
