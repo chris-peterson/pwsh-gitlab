ffb3c5f65d232759c9407f9191c814c2ec6b0ac1
Chris Peterson <chrispeterson@fastmail.com>
2025-12-29 14:20:27 -0800

    Fix PSAvoidAssignmentToAutomaticVariable

--- src/GitlabCli/Private/Transformations.ps1
+++ src/GitlabCli/Private/Transformations.ps1
@@ -1,5 +1,5 @@
 class TrueOrFalseAttribute : System.Management.Automation.ArgumentTransformationAttribute {
-    [object] Transform([System.Management.Automation.EngineIntrinsics] $_, $inputData) {
+    [object] Transform([System.Management.Automation.EngineIntrinsics] $engineIntrinsics, $inputData) {
         if ($inputData -is [bool]) {
             return $inputData
         }
